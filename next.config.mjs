22764ab0ea1291fcde060c835c673f2e2b74ef82
Greencloud <Greencloud>
2025-12-06 20:12:40 +0800

    fix: add turbopack in next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    turbopack: true,
+    turbopack: {
+      root: __dirname, // sets project root explicitly
+    },
+  },
 };
 
 export default nextConfig;
